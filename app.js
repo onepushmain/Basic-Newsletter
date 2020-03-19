063e696d858d1cdae63438c1abf81890ae475ce0
Luiz Fernando D <lfluiz.lf@gmail.com>
2020-03-19 18:49:59 -0300

    Created a Procfile and edditing the port im using

--- app.js
+++ app.js
@@ -69,4 +69,4 @@ app.post("/sucess", (req,res) => {
     res.redirect("/");
 })
 
-app.listen(port, () => console.log("Server UP on port " + port));
+app.listen(process.env.PORT || 3000, () => console.log("Server UP on port " + port));
